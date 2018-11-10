e060094354e8cc3d7f029f36d5abfdf2e52e1f92
DiaconuRares <40871918+DiaconuRares@users.noreply.github.com>
2018-11-10 16:19:55 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    
+    $(".cont").hide();
+    
+    $(".header>.link0").click(function() {
+        $(".cont").hide();
+        $(".cover").show();
+    });
+    
+    $(".header>.link1").click(function() {
+        $(".cont").hide();
+        $(".container0").show();
+    });
+    
+    $(".header>.link2").click(function() {
+        $(".cont").hide();
+        $(".container1").show();
+    });
+    
+    $(".header>.link3").click(function() {
+        $(".cont").hide();
+    });
+});
